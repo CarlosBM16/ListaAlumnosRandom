642eb8e271ea7dc27037e426223045bd069a8104
Carlos Bermúdez Mancebo <carlosbermudezmancebo@gmail.com>
2025-10-21 18:40:21 +0200

    Centered List and Button

--- app/src/main/java/com/example/listaalumnosrandom/MainActivity.kt
+++ app/src/main/java/com/example/listaalumnosrandom/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.listaalumnosrandom.ui.theme.ListaAlumnosRandomTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +28,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ListaAlumnosRandomTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            ListaAlumnosRandomTheme {ListaAlumnosCard()
+            }
+        }
+    }
+}
 
+@Composable
+fun ListaAlumnosCard() {
+    val listaInicial = listOf<String>("Carlos0", "Carlos1", "David Salvador", "Giovanni", "Iván", "Daniel", "David Romero")
+    var lista by remember { mutableStateOf(listaInicial) }
+
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box (
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 32.dp)
+        ){
+            Column {
+                lista.forEach { nombre ->
+                    Text(text = nombre)
                 }
             }
+
+        }
+
+
+        Button(onClick = {
+
+        }) {
+            Text("Elegir alumno")
         }
     }
 }
@@ -30,7 +68,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ListaAlumnosRandomTheme {
-
-    }
+    ListaAlumnosCard()
 }
